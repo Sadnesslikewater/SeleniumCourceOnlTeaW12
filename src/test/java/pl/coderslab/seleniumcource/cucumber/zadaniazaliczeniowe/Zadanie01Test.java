40e2836e5903554ff39bcb2453412859e1ed02e4
sadnesslikewater <sadness.like.water@gmail.com>
2022-01-03 18:29:52 +0100

    zadaniezaliczeniowetest

--- src/test/java/pl/coderslab/seleniumcource/cucumber/zadaniazaliczeniowe/Zadanie01Test.java
+++ src/test/java/pl/coderslab/seleniumcource/cucumber/zadaniazaliczeniowe/Zadanie01Test.java
@@ -1,4 +1,11 @@
 package pl.coderslab.seleniumcource.cucumber.zadaniazaliczeniowe;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/cucumber/features/zadanie01.feature",
+        plugin = {"pretty","html:out"})
 public class Zadanie01Test {
 }
