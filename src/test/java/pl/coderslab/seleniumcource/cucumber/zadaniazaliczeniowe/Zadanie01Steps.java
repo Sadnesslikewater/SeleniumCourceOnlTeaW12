a4ccfed081e85242c21bc2e6f699b740d19a95f7
sadnesslikewater <sadness.like.water@gmail.com>
2022-01-05 21:41:34 +0100

    Zadanie01Stepslast

--- src/test/java/pl/coderslab/seleniumcource/cucumber/zadaniazaliczeniowe/Zadanie01Steps.java
+++ src/test/java/pl/coderslab/seleniumcource/cucumber/zadaniazaliczeniowe/Zadanie01Steps.java
@@ -8,17 +8,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pl.coderslab.seleniumcource.pageobject.CreateAnAccountPage;
 
 import java.time.Duration;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Zadanie01Steps {
     private WebDriver driver;
     private Object UserData;
-    //private final Object UserData;
-    //public Zadanie01Steps(Object userData) {
-    //UserData = userData;
-
 
     @Given("^Web page opened (.*)$")
     public void WebPageOpenedInBrowser(String url) {
@@ -27,43 +27,48 @@ public class Zadanie01Steps {
         this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         this.driver.get(url);
     }
+
     @And("^First key sign in pressed$")
     public void firstSignInBtn() {
         WebElement firstSignInBtn = driver.findElement(By.id("_desktop_user_info"));
         firstSignInBtn.click();
     }
+
     @And("^Email address entered in input box$")
     public void emailAddressEntered() {
         WebElement emailAddressInput = driver.findElement(By.xpath("//*[@id=\"login-form\"]/section/div[1]/div[1]/input"));
         emailAddressInput.clear();
         emailAddressInput.sendKeys("jankowalski@email.com");
     }
+
     @And("^Password entered in input box$")
     public void passwordEntered() {
         WebElement passwordInput = driver.findElement(By.xpath("//*[@id=\"login-form\"]/section/div[2]/div[1]/div/input"));
         passwordInput.clear();
         passwordInput.sendKeys("Password123");
     }
+
     @And("^Second key sign in pressed$")
     public void secondKeySignInPressed() {
         WebElement secondSignInBtn = driver.findElement(By.id("submit-login"));
         secondSignInBtn.click();
     }
+
     @When("^Addresses button pressed$")
     public void addressesButtonPressed() {
         WebElement addressesBtn = driver.findElement(By.xpath("//*[@id=\"addresses-link\"]/span/i"));
         addressesBtn.click();
     }
+
     @And("^Create new address button pressed$")
     public void createNewAddressButtonPressed() {
         WebElement createNewAddressBtn = driver.findElement(By.xpath("//*[@id=\"content\"]/div[3]/a/span"));
         createNewAddressBtn.click();
     }
-    @Then("^Form filled with data: (.*), (.*), (.*), (.*), (.*), (.*)$")
-    public void formFilledWithData(String alias, String address, String city, int zipCode, String country, int phone) {
-        //pl.coderslab.seleniumcource.cucumber.zadaniazaliczeniowe.AddressData addressData = new AddressData(driver);
-        //pl.coderslab.seleniumcource.cucumber.zadaniazaliczeniowe.UserData userData = new pl.coderslab.seleniumcource.cucumber.zadaniazaliczeniowe.UserData()
-                UserData userdata = new UserData()
+
+    @Then("^Form filled with data: (.*), (.*), (.*), (.*), (.*), (.*) and data checked$")
+    public void formFilledWithData(String alias, String address, String city, String zipCode, String country, int phone) {
+        UserData userData = new UserData()
                 .setAlias(alias)
                 .setAddress(address)
                 .setZipCode(zipCode)
@@ -72,22 +77,34 @@ public class Zadanie01Steps {
                 .setPhone(phone);
 
         AddressData addressData = new AddressData(driver);
-        addressData.formFilledWithData((pl.coderslab.seleniumcource.cucumber.zadaniazaliczeniowe.AddressData) UserData);
+        assertEquals(alias, userData.getAlias());
+        assertEquals(address, userData.getAddress());
+        assertEquals(zipCode, userData.getZipCode());
+        assertEquals(city, userData.getCity());
+        assertEquals(country, userData.getCountry());
+        assertEquals(phone, userData.getPhone());
+        addressData.formFilledWithData(userData);
     }
-    @And("^Correctness of data checked$")
-    public void correctnessOfDataChecked() {
-        WebElement correctnessMsg = driver.findElement(By.xpath("//article(@class='alert']"));
-        String correctnessMsgText = correctnessMsg.getText();
-        assertEquals("Address successfully added!", correctnessMsgText);
-    }
+
     @And("^Save button pressed$")
     public void saveButtonPressed() {
         WebElement saveBtn = driver.findElement(By.xpath("//*[@id=\"content\"]/div/div/form/footer/button"));
         saveBtn.click();
     }
+
+    @And("^New account deleted$")
+    public void deleteNewAccount() {
+        driver.get("https://mystore-testlab.coderslab.pl/index.php?controller=addresses");
+        WebElement deleteAccountBtn = driver.findElement(By.xpath("/html/body/main/section/div/div/section/section/div[2]/article/div[2]/a[2]/span"));
+        deleteAccountBtn.click();
+    }
+
+    @And("^Account deletion message displayed$")
+    public void deleteAccountMsg() {
+        WebElement successMessage = driver.findElement(By.xpath("/html/body/main/section/div/div/section/section/aside/div/article/ul/li"));
+        String successMessageText = successMessage.getText();
+        assertEquals("Address successfully deleted!", successMessageText);
+    }
 }
-
-
-
 
 
